0d3668f47a64d5a91901e203eb023f6481880050
Tentsu <tentsuh@gmail.com>
2015-10-24 16:16:32 +0300

    Spaces to tabs

--- SheetLocalizator.js
+++ SheetLocalizator.js
@@ -1,110 +1,110 @@
 (function(window) {
-    'use strict';
+	'use strict';
 
-    var KEY_COLUMN = 'A';
-    var TITLE_ROW = 1;
-    var SHEET_URL = 'https://spreadsheets.google.com/feeds/cells/SHEET_ID/od6/public/basic?hl=en_US&alt=json';
+	var KEY_COLUMN = 'A';
+	var TITLE_ROW = 1;
+	var SHEET_URL = 'https://spreadsheets.google.com/feeds/cells/SHEET_ID/od6/public/basic?hl=en_US&alt=json';
 
-    var SheetLocalizator = function(sheetId, language, callback) {
-        _getSheet(sheetId, function(data) {
-            data = JSON.parse(data);
+	var SheetLocalizator = function(sheetId, language, callback) {
+		_getSheet(sheetId, function(data) {
+			data = JSON.parse(data);
 
-            callback(_parseLocalizations(data.feed.entry, language));
-        });
-    };
+			callback(_parseLocalizations(data.feed.entry, language));
+		});
+	};
 
-    function _getSheet(id, callback) {
-        var r = new XMLHttpRequest();
-        r.open("GET", _getUrl(id), true);
-        r.onreadystatechange = function() {
-            if (r.readyState != 4 || r.status != 200) return;
-            callback(r.responseText);
-        };
-        r.send();
-    }
+	function _getSheet(id, callback) {
+		var r = new XMLHttpRequest();
+		r.open("GET", _getUrl(id), true);
+		r.onreadystatechange = function() {
+			if (r.readyState != 4 || r.status != 200) return;
+			callback(r.responseText);
+		};
+		r.send();
+	}
 
-    function _parseLocalizations(cells, language) {
-        language = language || 'FI';
+	function _parseLocalizations(cells, language) {
+		language = language || 'FI';
 
-        var titles = _parseTitles(cells);
-        var languageColumn = _getLanguageColumn(titles, language);
+		var titles = _parseTitles(cells);
+		var languageColumn = _getLanguageColumn(titles, language);
 
-        if (!languageColumn) {
-            return;
-        }
+		if (!languageColumn) {
+			return;
+		}
 
-        var keys = _getKeys(titles, cells);
-        var localizations = {};
+		var keys = _getKeys(titles, cells);
+		var localizations = {};
 
-        for (var i = titles.length; i < cells.length; i++) {
-            var cell = _getCell(cells[i]);
+		for (var i = titles.length; i < cells.length; i++) {
+			var cell = _getCell(cells[i]);
 
-            if (cell.column === languageColumn) {
-                localizations[keys[cell.row]] = cell.title;
-            }
-        }
+			if (cell.column === languageColumn) {
+				localizations[keys[cell.row]] = cell.title;
+			}
+		}
 
-        return localizations;
-    }
-
-
-    function _parseTitles(cells) {
-        var titles = [];
-
-        for (var i = 0; i < cells.length; i++) {
-            titles.push(_getCell(cells[i]));
-
-            // Check if next cell is already something else than title
-            if (cells[i + 1] && cells[i + 1].title.$t.match(/\d+/).join() > TITLE_ROW) {
-                return titles;
-            }
-        }
-
-        return titles;
-    }
+		return localizations;
+	}
 
 
-    function _getUrl(id) {
-        return SHEET_URL.replace('SHEET_ID', id);
-    }
+	function _parseTitles(cells) {
+		var titles = [];
 
-    function _getCell(cell) {
-        var letters = /[a-zA-Z]+/;
-        var numbers = /\d+/;
+		for (var i = 0; i < cells.length; i++) {
+			titles.push(_getCell(cells[i]));
 
-        return {
-            'title': cell.content.$t,
-            'column': cell.title.$t.match(letters).join(),
-            'row': cell.title.$t.match(numbers).join()
-        };
-    }
+			// Check if next cell is already something else than title
+			if (cells[i + 1] && cells[i + 1].title.$t.match(/\d+/).join() > TITLE_ROW) {
+				return titles;
+			}
+		}
 
-
-    function _getLanguageColumn(titles, language) {
-        for (var i = 0; i < titles.length; i++) {
-            if (titles[i].title === language) {
-                return titles[i].column;
-            }
-        }
-
-        return null;
-    }
+		return titles;
+	}
 
 
-    function _getKeys(titles, cells) {
-        var keys = {};
+	function _getUrl(id) {
+		return SHEET_URL.replace('SHEET_ID', id);
+	}
 
-        for (var i = titles.length; i < cells.length; i++) {
-            var cell = _getCell(cells[i]);
+	function _getCell(cell) {
+		var letters = /[a-zA-Z]+/;
+		var numbers = /\d+/;
 
-            // Check if KEY column
-            if (cell.column === KEY_COLUMN) {
-                keys[cell.row] = cell.title;
-            }
-        }
+		return {
+			'title': cell.content.$t,
+			'column': cell.title.$t.match(letters).join(),
+			'row': cell.title.$t.match(numbers).join()
+		};
+	}
 
-        return keys;
-    }
 
-    window.SheetLocalizator = SheetLocalizator;
+	function _getLanguageColumn(titles, language) {
+		for (var i = 0; i < titles.length; i++) {
+			if (titles[i].title === language) {
+				return titles[i].column;
+			}
+		}
+
+		return null;
+	}
+
+
+	function _getKeys(titles, cells) {
+		var keys = {};
+
+		for (var i = titles.length; i < cells.length; i++) {
+			var cell = _getCell(cells[i]);
+
+			// Check if KEY column
+			if (cell.column === KEY_COLUMN) {
+				keys[cell.row] = cell.title;
+			}
+		}
+
+		return keys;
+	}
+
+	window.SheetLocalizator = SheetLocalizator;
 })(window);
